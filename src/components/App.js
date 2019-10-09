bfce0979d18f1fc186313cd3f89be90fc90951e2
saleryan <saleryan@gmail.com>
2019-10-09 18:40:46 +0000

    Subject: Added initial navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
-import './App.css';
+import '../index.css';
 import {connect} from 'react-redux' 
 import {getInitialData} from '../actions/shared'
+import Login from './Login';
+import {Route, BrowserRouter} from 'react-router-dom'
+import NavBar from './NavBar';
 class App extends Component {
   
   componentDidMount() {
     this.props.dispatch(getInitialData());
   }
+  
   render() {
     return (
-      <div className="App">
-        <div className="App-header">        
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+   	  <BrowserRouter>
+      <div>
+      <h3 className="center header"> React App </h3>
+      <NavBar/>
+      <hr />
+      <div className="container center">
+       	<Route path='/' exact component={Login}/>
       </div>
+	  </div>
+	</BrowserRouter>
+	
     );
   }
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+export default function NavBar() {
+ return (
+ 	<nav className='nav'>
+    <ul>
+   	<li>
+    <NavLink to='/' exact activeClassName='active'> Home </NavLink>
+   </li>
+   <li>
+    <NavLink to='/add' exact activeClassName='active'>New Question </NavLink>
+   </li>
+   <li>
+    <NavLink to='/leaderboard' exact activeClassName='active'>Leader Board </NavLink>
+   </li>
+    <li>
+    <NavLink to='/logout' exact activeClassName='active'>Logout</NavLink>
+   </li>
+   </ul>
+    </nav>
+  
+   
+ )
+  
+}
