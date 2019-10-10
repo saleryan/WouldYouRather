45868d4e2930302a2b6c4df777abac80553588b4
saleryan <saleryan@gmail.com>
2019-10-10 23:23:39 +0000

    Subject: Display answered and unanswered questions.

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -3,23 +3,47 @@ import {connect} from 'react-redux';
 import Question from './Question';
 
 class QuestionList extends Component {
+ state = {
+  questionIds:this.props.answered 
+ }
+
  render() {
-  return  (<ul className="questions">
-           {this.props.questionIds
+  
+  return  (<div>
+           <div style={{display: "flex"}}>
+           	<button className="btn" onClick={()=> this.setState({questionIds: this.props.unanswered})}>Unanswered Questions </button>
+           	<button className="btn" onClick={()=> this.setState({questionIds: this.props.answered})}>Answered Questions </button>
+           </div>
+           <ul className="questions">
+           {this.state.questionIds
            			.map(id => 
             			<li key={id}> <Question key={id} id={id} /> </li>
-          				)
+           
+			)
            }
           
           
-          </ul>) ;
+          </ul></div>) ;
  	}
 }
 
 function mapStateToProps(state) {
- const questionIds = Object.keys(state.questions);
+  const user = state.users[state.authUser];
+ 
+  const answered = [];
+  const unanswered =[];
+  Object.keys(state.questions)
+   .forEach(questionId => {
+     if (user.answers.hasOwnProperty(questionId) ) {
+       answered.push(questionId);
+     } else {
+       unanswered.push(questionId);
+     }
+   });                                                      
+                                                        
   return {
-    questionIds
+    answered: answered,
+    unanswered: unanswered
   }
 }
 
