d5497eff187e2a6c2a93ea568a7d3b867e2dde44
saleryan <saleryan@gmail.com>
2019-10-12 00:39:22 +0000

    Subject: styling and completed QuestionDetail Component.

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,35 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Question extends Component {
- 
+
     render() {
-       const {userName, avatarURL, questionId, optionOne} = this.props;
+        const { username, avatarURL, questionId, optionOne } = this.props;
         return (<div className="card">
             <div className="card-title">
-               <span> {userName} asks:</span>
+                <span> {username} asks:</span>
             </div>
             <div className="card-body">
-                <div className='avatar'  style={{ backgroundImage:  `url(${avatarURL})`}}></div>
+                <div className='avatar' style={{ backgroundImage: `url(${avatarURL})` }}></div>
                 <div>
-                   
-					 <div style={{width:"250px",overflow:"hidden",textOverflow:"ellipsis",whiteSpace:"nowrap"}}>Would you rather {optionOne}?</div>
-                     <Link className="btn" to={`/question/${questionId}`}> View Poll </Link>
+
+                    <div className="text-ellipsis">Would you rather {optionOne}?</div>
+                    <Link className="btn" to={`/question/${questionId}`}> View Poll </Link>
                 </div>
             </div>
         </div>);
     }
 }
 
-function mapStateToProps(state, {id}) {
-const user = state.users[state.questions[id].author];
-return {
- 	userName:  user.name,
-    avatarURL: user.avatarURL,
- 	questionId: id,
-    optionOne:state.questions[id].optionOne.text
-	}
+function mapStateToProps(state, { id }) {
+    const user = state.users[state.questions[id].author];
+    return {
+        username: user.name,
+        avatarURL: user.avatarURL,
+        questionId: id,
+        optionOne: state.questions[id].optionOne.text
+    }
 }
 
 export default connect(mapStateToProps)(Question);
--- src/components/QuestionDetail.js
+++ src/components/QuestionDetail.js
@@ -3,66 +3,86 @@ import { connect } from 'react-redux';
 import { saveAnswer } from '../actions/shared'
 
 class QuestionDetail extends Component {
-    state={
-    	value:this.props.optionOne
+    state = {
+        value: this.props.optionOne
     }
-  	answerQuestion = (e) => {
-      e.preventDefault();
-      this.props.dispatch(saveAnswer(this.props.authUserId, this.props.questionId, this.state.value));
-            
+    answerQuestion = (e) => {
+        e.preventDefault();
+        this.props.dispatch(saveAnswer(this.props.authUserId, this.props.questionId, this.state.value));
+
     }
-	handleChange=(e) => {
-      const value = e.target.value;
-      this.setState({value: value});
+    handleChange = (e) => {
+        const value = e.target.value;
+        this.setState({ value: value });
     }
 
     render() {
-        const { userName, avatarURL, optionOne, optionTwo, totalVotes, answer } = this.props;
-        return (<div className="card">
-                <div className="card-title">
-                    <span> {userName} asks:</span>
-                </div>
-                <div className="card-body">
+        const { username, avatarURL, optionOne, optionTwo, totalVotes, answer } = this.props;
+        const optionOnePercent = parseInt(optionOne.votes.length / totalVotes * 100, 10);
+        const optionTwoPercent = parseInt(optionTwo.votes.length / totalVotes * 100, 10);
+        const optionOneCount = `${optionOne.votes.length} out of ${totalVotes} votes`;
+        const optionTwoCount = `${optionTwo.votes.length} out of ${totalVotes} votes`;
 
-                    <div className='avatar' style={{ backgroundImage: `url(${avatarURL})` }}></div>
-					{!answer?<div>
-                        <form className="detail-form" onSubmit={this.answerQuestion}>
-                            Would you rather ...
-							<p/>
-					        <input name="answer" type="radio" id="optionOne" value="optionOne" onChange={this.handleChange}/>
-							<span>{optionOne.text}</span>
-							<p/>
-						    <input type="radio" name="answer" id="optionTwo" value="optionTwo"  onChange={this.handleChange}/>
-							<span>{optionTwo.text}</span>
-                            <button className="btn" type="submit">Submit</button>
-                        </form>
-                    </div>:<div> 
-				     <span>Results</span>
-					 <p/>
-					 <span style={{backgroundColor: "optionOne"===answer?"red":"white" }}>Would you rather {optionOne.text}?</span>
-					 <p/>
-					 <span style={{backgroundColor: "optionTwo"===answer?"red":"white" }}> Would you rather {optionTwo.text}?</span>
-					</div>}
-                </div>
-            </div>);
+        return (<div className="card">
+            <div className="card-title">
+                <span> {username} asks:</span>
+            </div>
+            <div className="card-body">
+
+                <div className='avatar' style={{ backgroundImage: `url(${avatarURL})` }}></div>
+                {!answer ? <div>
+                    <form className="detail-form" onSubmit={this.answerQuestion}>
+                        <strong>Would you rather ...</strong>
+                        <p />
+                        <input name="answer" type="radio" id="optionOne" value="optionOne" onChange={this.handleChange} />
+                        <span>{optionOne.text}</span>
+
+                        <input type="radio" name="answer" id="optionTwo" value="optionTwo" onChange={this.handleChange} />
+                        <span>{optionTwo.text}</span>
+                        <button className="btn" type="submit">Submit</button>
+                    </form>
+                </div> :
+                    <div className="results-info">
+
+                        <h3>Results:</h3>
+                        <div className="result" style={{ borderWidth: "optionOne" === answer ? "5px" : "1px" }}>
+                            {"optionOne" === answer && <div className="voted"> Your vote</div>}
+                            <strong>Would you rather {optionOne.text}?</strong>
+                            <div className="progress-bar">
+                                <div style={{ width: optionOnePercent + '%' }} />
+                            </div>
+                            <strong className="center">{optionOneCount}</strong>
+                        </div>
+                        <div className="result" style={{ borderWidth: "optionTwo" === answer ? "5px" : "1px" }}>
+                            {"optionTwo" === answer && <div className="voted"> Your vote</div>}
+                            <strong> Would you rather {optionTwo.text}?</strong>
+                            <div className="progress-bar">
+                                <div style={{ width: optionTwoPercent + '%' }} />
+                            </div>
+                            <strong className="center">{optionTwoCount}</strong>
+
+                        </div>
+                    </div>}
+            </div>
+        </div>);
     }
 }
 
-function mapStateToProps(state,  props ) {
+function mapStateToProps(state, props) {
     const { id } = props.match.params
     const question = state.questions[id];
-	const user = state.users[question.author];
+    const user = state.users[question.author];
     const authUser = state.users[state.authUser];
-	return {
-      authUserId: authUser.id,
-      userName:user.author,
-      avatarURL:user.avatarURL,
-      questionId: id,
-	  optionOne: {...question.optionOne},
-      optionTwo: {...question.optionTwo},
-      totalVotes: question.optionOne.votes.length + question.optionTwo.votes.length,
-      answer: authUser.answers[question.id]
-	}
+    return {
+        authUserId: authUser.id,
+        username: user.name,
+        avatarURL: user.avatarURL,
+        questionId: id,
+        optionOne: { ...question.optionOne },
+        optionTwo: { ...question.optionTwo },
+        totalVotes: question.optionOne.votes.length + question.optionTwo.votes.length,
+        answer: authUser.answers[question.id]
+    }
 }
 
 export default connect(mapStateToProps)(QuestionDetail);
