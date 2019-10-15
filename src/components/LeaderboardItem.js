13a8032636ef739145930c6ce09c9ae900d333ef
saleryan <saleryan@gmail.com>
2019-10-15 00:08:54 +0000

    feat: styling for leaderboard component

--- src/components/LeaderboardItem.js
+++ src/components/LeaderboardItem.js
@@ -7,16 +7,22 @@ class LeaderboardItem extends Component {
         const score = askedQuestions + answeredQuestions;
         return (<div className="leaderboard-item">
             <div className='avatar' style={{ backgroundImage: `url(${avatarURL})` }}></div>
-            <div>
-                <strong>{username}</strong>
+            <div className="leaderboard-info">
+                <h2>{username}</h2>
 				<div>
-                <div><span>Answered Questions</span> <span>{answeredQuestions}</span></div>
-                <div><span>Created Questions</span><span>{askedQuestions}</span></div>
+                  <div className="leaderboard-info-question-container">
+                          <span>Answered Questions</span> 
+                          <span className="leaderboard-info-question">{answeredQuestions}</span>
+                  </div>
+                  <div className="leaderboard-info-question-container">
+                          <span>Created Questions</span>
+                          <span className="leaderboard-info-question">{askedQuestions}</span>
+                  </div>
 				</div>
             </div>
-            <div>
-                <div>score</div> 
-				<div>{score}</div>
+            <div className="score-container">
+                <div className="score-title">score</div> 
+				<div className="score">{score}</div>
             </div>
         </div>);
 
